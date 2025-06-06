c46a8fa31260ad56a9667b191918e4b67b7184f6
adityachandra512 <143398217+adityachandra512@users.noreply.github.com>
2025-06-06 16:09:33 +0530

    updated the layout

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,13 @@ import Experience from "@/components/main/Experience";
 import Projects from "@/components/main/Project";
 import Image from "next/image";
 import Footer from "@/components/main/Footer";
+import Navbar from "@/components/main/Navbar";
 
 export default function Home() {
   return (
     <main className="h-full w-full relative">
       <div className="flex flex-col gap-20 relative z-10">
+        <Navbar />
         <Hero />  
         <About />
         <Skills />
